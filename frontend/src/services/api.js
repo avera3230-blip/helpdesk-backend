217cd6dad3feb41b250d608e3b608a35ba7105bf
elviszambrano8344-ops <ezambrano8344@utm.edu.ec>
2026-07-27 14:36:55 -0500

    Conectar frontend con Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/tickets",
+  baseURL: "https://helpdesk-fullstack.onrender.com/api/tickets",
 });
 
 export default API;
